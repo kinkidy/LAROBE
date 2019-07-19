38c73fc816bd686543c668ecf521beffa9b52e70
kinkidy <ahmadidi007@gmail.com>
2019-07-19 22:53:38 +0100

    slider added

---
+++ js grammar/entrypoint.js
@@ -0,0 +1,25 @@
+
+// export function Mouse () {
+//      this.x = 0; this.y = 0; 
+// }
+
+// export {add, substract,divide};
+
+// function add (a, b) {
+//     return a + b;
+// }
+
+// function substract (c,d) {
+//     return c - d;
+// }
+
+// function divide (e,f) {
+//     return e/f;
+// }
+
+"use strict"
+
+var variable = 1;
+
+delete variable;
+
