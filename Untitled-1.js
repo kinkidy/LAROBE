f238807386335d87ae70a2474987ed71e227a6d0
kinkidy <ahmadidi007@gmail.com>
2019-12-03 09:42:16 +0100

    New design

--- Untitled-1.js
+++ Untitled-1.js
@@ -3,3 +3,15 @@
 $('#exampleSlider').multislider({
     interval:false
 });
+
+
+//decalring varible
+const menuBtn = document.querySelectorAll('a');
+const header = document.getElementsByClassName ('header');
+
+const menuBtn2 = menuBtn[1];
+
+//event listener
+menuBtn2.addEventListener('click', () => {
+    header.style.backgroundColor('red');
+});
